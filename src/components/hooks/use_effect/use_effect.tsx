16a763bfc7b38ebfb518e82ea14ac2df5f8dab01
unknown <marlon_tancredo@hotmail.com>
2023-09-04 18:37:36 +0100

    feat: implementing useEffect hookt

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,34 @@
-// make an api call using useEffect
+import { useEffect, useState } from "react";
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const APICall = () => {
+  const [json, setJson] = useState<Todo | null>(null);
 
-    // Your code here!
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+    setJson(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>Title: {json?.title}</h2>
+      <p>User id: {json?.userId}</p>
+      <p>Id: {json?.id}</p>
+      <p>Completed: {JSON.stringify(json?.completed)}</p>
+    </>
+  );
+};
