ca410a956c1210f5214df307084e12fd2a156e77
unknown <marlon_tancredo@hotmail.com>
2023-09-04 18:41:22 +0100

    feat: implementing useState hooks

--- src/components/hooks/use_state/count_cats.tsx
+++ src/components/hooks/use_state/count_cats.tsx
@@ -1,16 +1,21 @@
-export function CountCats() {
+import { useState } from "react";
 
-  const cats = ["🐈"].length;
+const catsArr: string[] = ["🐈"];
+
+export function CountCats() {
+  const [cats, setCats] = useState(catsArr);
+
+  const incrementCats = () => {
+    setCats([...cats, "🐈"]);
+  };
 
   return (
     <>
-    <h2>useState</h2>
+      <h2>useState</h2>
 
-    <p>{cats}</p>
+      <p>{cats.length}</p>
 
-    <button>
-      There are ... cats 🥳
-    </button>
+      <button onClick={incrementCats}>There are ... cats 🥳</button>
     </>
   );
 }
